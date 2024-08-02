06c281424bf165a5e17a639931b53032743b9355
davatji <davatji@gmail.com>
2024-08-02 14:36:28 +0700

    debug mode -> true

--- betlehem_web_demo/settings.py
+++ betlehem_web_demo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+bp94#sbs^11yp6do15n&k%52@iky#gk!#-tj85%o4%vp+@kzl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
